a9ce44901b04c3a33dde5f8b1e409282be6fdf5f
theogolova <tgolova@udesa.edu.ar>
2024-06-23 23:36:17 -0300

    mostrar un producto : desarrollo completo + validaciones

--- controllers/productocontroller.js
+++ controllers/productocontroller.js
@@ -3,6 +3,61 @@ const op = db.Sequelize.Op;
 const { validationResult } = require("express-validator");
 
 const productContoller = {
+    delete: function(req, res) {
+        let form = req.body;
+        
+        let filtrado = {
+          where: {
+            id: form.id
+          }
+        }  
+        
+       if (req.session.user != undefined) {
+            let id = req.session.user.id;
+            if (form.idUsuario == id) {
+                db.Product.destroy(filtrado)
+                .then((result) => {
+                  return res.redirect("/");
+                })
+                .catch((err) => {
+                  return console.log(err);
+                });
+            }
+            else{
+                return res.redirect("/users/profile/id/" + id);
+            }
+        }
+        else{
+            return res.redirect("/users/login");
+        }  
+    },  
+    product: function (req, res) {
+
+        let id = req.params.id;
+
+        let criterio = {
+            include: [
+                {association: "usuario"},
+                {association: "comentarios",
+                include: [{association: "usuario"}]
+                }
+            ],
+            order: [[{model: db.Comentario, as: "comentarios"}, "createdAt", "DESC"]]
+        };
+
+        let condition = false;
+
+        db.Product.findByPk(id, criterio)
+        .then(function(results){
+            if (req.session.user != undefined && req.session.user.id == results.usuario.id){
+                condition = true;
+            }
+            return res.render("product", {productos: results, comentarios: results.comentarios, condition: condition})
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    },
 
     create: function(req, res) {
         if(req.session.user != undefined){
--- routes/producto.js
+++ routes/producto.js
@@ -12,11 +12,26 @@ let validacionAgregar = [
         .notEmpty().withMessage('Debes agregar una imagen').bail()
         .isURL().withMessage('La URL de la imagen no es valida')
 ]
+let validacionComentario = [
+    body('comentario')
+        .notEmpty().withMessage('Debes completarlo').bail()
+        .isLength({min: 5}).withMessage("El comentario tiene que por lo menos tener 5 caracteres como minimo")
+]
 
 
+
+router.get('/id/:id', productContoller.product);
+router.post('/id/:id',validacionComentario, productContoller.comment);
 
 router.get('/add', productContoller.create);
 router.post('/add',validacionAgregar, productContoller.store);
 
+
+router.post('/editProduct', productContoller.formUpdate);
+router.post('/edit',validacionAgregar, productContoller.update);
+
+router.post('/delete', productContoller.delete);
+
 module.exports = router;
+
 
