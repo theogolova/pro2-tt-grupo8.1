01a72867cc3e5c08e6a380e0e8128de9b4ac6360
Felimaison <142600234+Felimaison@users.noreply.github.com>
2024-06-23 23:18:45 -0300

    login: desarrollo completo as validaciones y controles de acceso

--- controllers/perfilcontroller.js
+++ controllers/perfilcontroller.js
@@ -44,5 +44,49 @@ const perfilContoller = {
 
 
 },
+login: function(req, res, next){
 
+    if (req.session.user != undefined) {
+        return res.redirect("/users/profile/id/" + req.session.user.id)
+    } else {
+        return res.render("login", {title: "Login"})
+    }
+},
+
+loginUser: (req, res, next) => {
+
+    let form = req.body;
+    let errors = validationResult(req)
+
+    if (errors.isEmpty()) {
+        
+        let filtrar = {
+            where: [
+            {mail: form.email}
+            ]
+        }
+
+    db.Usuario.findOne(filtrar)
+        .then((result) => {
+            if (result != null) {
+                
+                req.session.user = result;
+                if (form.remember != undefined) {
+                    res.cookie("userId", result.id, {maxAge: 1000 * 60 * 35})
+                }
+                return res.redirect("/users/profile/id/" + result.id);
+            } 
+            else {
+                return res.redirect("/users/login");
+            }
+
+        })
+        .catch((err) => {
+            return console.log(err);
+        });
+    }
+    else{
+        res.render('login', {title: "Login", errors: errors.mapped(),  old: req.body, user: req.session.user});
+    }
+},
 }
