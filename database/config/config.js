cdd6a55ea9218e162940380407fc0e7811e79ca3
theogolova <tgolova@udesa.edu.ar>
2024-06-23 23:55:52 -0300

    ultimando detalles

--- database/config/config.js
+++ database/config/config.js
@@ -1,10 +1,9 @@
 module.exports = {
-    "development": {
-      "username": "root",
-      "password": "",
-      "database": "databaseproyecto",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
-    }
+  "development": {
+  "username": "root",
+  "password": "root", 
+  "database": "databaseproyecto",
+  "host": "127.0.0.1",
+  "dialect": "mysql",
   }
-  
+}
